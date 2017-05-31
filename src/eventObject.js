0c00e5d5ce14cbaad42494f1e2f1692a562e0668
ManOfJam <sbttaylor98@gmail.com>
2017-05-31 21:58:32 +0100

    Improved eventObject

--- src/eventObject.js
+++ src/eventObject.js
@@ -4,15 +4,40 @@ class EventObject {
 	}
 
 	on(events, callbacks) {
-		events = (Array.isArray(events) ? events : [events]).filter(event => typeof event === "string");
-		callbacks = (Array.isArray(callbacks) ? callbacks : [callbacks]).filter(callback => typeof callback === "function");
 
-		if(events.length && callbacks.length)
-			for(let event of events)
+		events = (Array.isArray(events) ? events : [events])
+			.filter(event => typeof event === "string");
+
+		callbacks = (Array.isArray(callbacks) ? callbacks : [callbacks])
+			.filter(callback => typeof callback === "function");
+
+		if(events.length && callbacks.length) {
+			for(const event of events) {
 				this.events[event] = (this.events[event] || (this.events[event] = [])).concat(callbacks);
+			}
+		}
 	}
 
 	off(events, callbacks) {
+
+		events = (Array.isArray(events) ? events : [events])
+			.filter(event => typeof event === "string");
+
+		callbacks = (Array.isArray(callbacks) ? callbacks : [callbacks])
+			.filter(callback => typeof callback === "function" || typeof callbackIndex);
+
+		if(events.length && callbacks.length) {
+			for(const event of events) {
+				for(const callback of callbacks) {
+					const callbackIndex = eventsList.findIndex(curCallback => curCallback === callback || curCallback.name === callback);
+
+					if(callbackIndex !== -1) {
+						this.events[e].splice(callbackIndex,);
+						if(!this.events[e].length) delete this.events[e][event];
+					}
+				}
+			}
+		}
 	}
 
 	trigger() {
