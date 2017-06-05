92bd38e32b319e907abb3d933cd79bb37f29f0cf
ManOfJam <sbttaylor98@gmail.com>
2017-06-05 02:33:01 +0100

    Added Spatial component, 'commented-out' Transform and Size components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 const components = {
 	Motion: require("./motion.js"),
-	Size: require("./size.js"),
-	Transform: require("./transform.js")
+	Spatial: require("./spatial.js")
+	//Size: require("./size.js"),
+	//Transform: require("./transform.js")
 };
 
 module.exports = components;
