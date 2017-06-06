cdb9df2902f9a9ffcc9ccccaba0cfc2f081a7c59
ManOfJam <sbttaylor98@gmail.com>
2017-06-06 04:47:05 +0100

    Moved all testing into demo folder, deleted index.html, added to .gitignore.

--- gulpfile.js
+++ gulpfile.js
@@ -32,5 +32,17 @@ gulp.task("host", function() {
 });
 
 gulp.task("dev", function() {
+	if(!fs.existsSync("demo")) {
+		fs.mkdirSync("demo");
+	}
+
+	if(!fs.existsSync("demo/index.html")) {
+		fs.writeFile("demo/index.html", "<!DOCTYPE html><html lang=\"en-gb\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>ECS | Demo</title></head><body><canvas id=\"canvas\" style=\"border: 1px solid #000; display: block; margin: 72px auto 0;\"></canvas><script src=\"/build/build.js\"></script><script src=\"demo.js\"></script></body></html>");
+	}
+
+	if(!fs.existsSync("demo/demo.js")) {
+		fs.writeFile("demo/demo.js", "");
+	}
+
 	sequence("watch", "host");
 });
