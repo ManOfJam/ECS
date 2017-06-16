74aedcafb368df70d21a8495fd3ff699a5a194f0
ManOfJam <sbttaylor98@gmail.com>
2017-06-16 03:03:09 +0100

    Updated gulp tasks, removed watchify and run-sequence as dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,37 @@
+const browserify = require("browserify");
 const fs = require("fs");
 const gulp = require("gulp");
+const stream = require("vinyl-source-stream");
 const webserver = require("gulp-webserver");
-const sequence = require("run-sequence");
-const browserify = require("browserify");
-const watchify = require("watchify");
 
-const b = browserify({
-	debug: true,
-	entries: "src",
-	cache: {},
-	packageCache: {},
-	plugin: [watchify]
+gulp.task("build", () => {
+	browserify("src")
+		.bundle()
+		.pipe(stream("build.js"))
+		.pipe(gulp.dest("dist"));
 });
 
-const build = function() {
-	if(!fs.existsSync("build"))
-		fs.mkdirSync("build");
-
-	b.bundle().pipe(fs.createWriteStream("build/build.js"));
-};
-
-gulp.task("build", build);
-
-gulp.task("watch", function() {
-	b.on("update", build);
-	build();
+gulp.task("watch", () => {
+	gulp.watch("src/**/*.js", ["build"]);
 });
 
-gulp.task("host", function() {
-	gulp.src("./").pipe(webserver());
-});
-
-gulp.task("dev", function() {
+gulp.task("serve", () => {
 	if(!fs.existsSync("demo")) {
 		fs.mkdirSync("demo");
 	}
 
 	if(!fs.existsSync("demo/index.html")) {
-		fs.writeFile("demo/index.html", "<!DOCTYPE html><html lang=\"en-gb\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>ECS | Demo</title></head><body><canvas id=\"demo\" style=\"border: 1px solid #000; display: block; margin: 72px auto 0;\"></canvas><script src=\"/build/build.js\"></script><script src=\"demo.js\"></script></body></html>");
+		fs.writeFile("demo/index.html", "<!DOCTYPE html><html lang=\"en-gb\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>ECS | Demo</title></head><body><canvas id=\"demo\" style=\"border: 1px solid #000; display: block; margin: 72px auto 0;\"></canvas><script src=\"/dist/build.js\"></script><script src=\"demo.js\"></script></body></html>\n");
 	}
 
 	if(!fs.existsSync("demo/demo.js")) {
 		fs.writeFile("demo/demo.js", "");
 	}
 
-	sequence("watch", "host");
+	gulp.src(".")
+		.pipe(webserver({
+			open: "demo"
+		}));
 });
+
+gulp.task("default", ["build", "watch", "serve"]);
